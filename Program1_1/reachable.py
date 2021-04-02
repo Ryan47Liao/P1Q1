9aecbe5b93a4f7749e8e3b4b09031969affdb0a1
Ryan47Liao <62736640+Ryan47Liao@users.noreply.github.com>
2021-04-02 11:36:43 +0800

    P1Q1 Completed

--- Program1_1/reachable.py
+++ Program1_1/reachable.py
@@ -1,11 +1,12 @@
 '''
 Created on Apr 1, 2021
 
-#Submitter: bliao2 (Liao, Leonidas)
+#Submitter: bliao2 (Liao, Bowen)
 #Partner: boiv (Vo, Boi Loc) 
 We certify that we worked cooperatively on this programming assignment, 
 according to the rules for pair programming 
 '''
+
 import prompt
 def read_graph(file)->dict:
     "This function takes in an opened file of graph and outputs a dictionary. "
@@ -13,9 +14,9 @@ def read_graph(file)->dict:
     for line in file:
         line = line.rstrip('\n')
         if line.split(";")[0] in Dict:
-            Dict[line.split(";")[0]].append(line.split(";")[1])
+            Dict[line.split(";")[0]].add(line.split(";")[1])
         else:
-            Dict[line.split(";")[0]] = list(line.split(";")[1])
+            Dict[line.split(";")[0]] = set(line.split(";")[1])
     return Dict
     
 def graph_as_str(graph:dict)->str:
@@ -26,11 +27,11 @@ def graph_as_str(graph:dict)->str:
     """
     STR = ""
     for SourceNode,DestinationNodes in sorted(list(graph.items())):
-        STR += SourceNode + "->" + str(DestinationNodes) + "\n"
+        STR += "  " + SourceNode + " -> " + str(sorted(list(DestinationNodes))) + "\n"
     print(STR)
     return STR
     
-def reachable(Graph:dict,start:str,tracing:bool = False):
+def reachablee(graph:dict,start:str,tracing:bool = False):
     """
     This function takes in a dictionary of the graph and a starting point, 
     it will output all the nodes that is reachable in the graph from the starting point.
@@ -42,7 +43,7 @@ def reachable(Graph:dict,start:str,tracing:bool = False):
         if tracing:
             print(f"reached set  = {reached}\nexploring set = {exploreing}",end='')
         node = exploreing[0]
-        for j in ([] if Graph.get(node) is None else Graph.get(node)):
+        for j in ([] if graph.get(node) is None else graph.get(node)):
             if j not in reached and j not in exploreing:
                 exploreing.append(j)
         reached.add(exploreing.pop(0))
@@ -54,8 +55,35 @@ after adding all nodes reachable directly from {node} but not already in reached
                 """)
     return reached
 
+
+def reachable(graph : {str:{str}}, start : str, trace : bool = False) -> {str}:
+    reached_set = set()
+    exploring_list = [start]
+    while exploring_list != []:
+        if trace:
+            print(f"reached set  = {reached_set}\nexploring set = {exploring_list}",end='')
+        start = exploring_list[0] 
+        if start not in reached_set:
+            if start in graph:
+                for items in graph[str(start)]:
+                    exploring_list.append(items)
+            reached_set.add(start)
+        exploring_list.pop(0)
+        if trace:
+            print(
+                f"""
+transfering node {start} from exploring set to the reached set
+after adding all nodes reachable directly from {start} but not already in reached,exploring = {exploring_list}
+                """)
+    return reached_set
+
 #User Prompt 
-def UI():
+
+        
+if __name__ == "__main__":
+    # Write script here
+    g1 = read_graph(open('graph1.txt'))
+    reachable(g1,'a')
     "An prompt based User Interface"
     file = None
     while file is None:
@@ -68,18 +96,178 @@ def UI():
     print("Graph: str (source node) -> [str] (sorted destination nodes)")
     graph_as_str(Graph)
     while True:
-        start = prompt.for_string(' Input one starting node (or input done)', 
+        start = prompt.for_string('Input one starting node (or input done)', 
                                    is_legal = lambda start: start in Graph or start == 'done'
                                    ,error_message= "Illegal: not a source node")
         if start == "done":
-            return 
-        tracing = prompt.for_bool("Input tracing algorithm option", True, "Enter False to disable tracing")
-        print(f"From the starting node {start}, its reachable nodes are:",reachable(Graph,start,tracing))
-        
-if __name__ == "__main__":
+            break 
+        trace = prompt.for_bool("Input tracing algorithm option", True, "Enter False to disable tracing")
+        print(f"From the starting node {start}, its reachable nodes are:",reachable(Graph,start,trace))
     #graph = read_graph(open('graph1.txt'))
     #graph_as_str(graph)
-    UI()
+    
+    # For running batch self-tests
+    
+    print()
+    import driver
+    driver.default_file_name = "bsc1.txt"
+#     driver.default_show_traceback = True
+#     driver.default_show_exception = True
+#     driver.default_show_exception_message = True
+    driver.driver()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     
     
 #Write a script at the bottom of this module (in if __name__ == '__main__':) that prompts the user to enter the file storing the graph and start node (rejecting any string that is not a source node in the graph) or the word done; calls these functions with the entered information to solve the problem, and print the appropriate information: the graph and the set containing all the node labels (body is 9 lines).
